df6e316520e58485873a4f08a5ac1a1f8713df5c
liob <hbwinther@metalabs.de>
2014-02-26 01:43:29 +0100

    Bootstrap baby

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 import os, ConfigParser, hashlib
-from flask import Flask, redirect, url_for, flash, request
+from flask import Flask, redirect, url_for, flash, request, render_template
 from flask.ext.login import LoginManager, login_user, logout_user, current_user
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -22,14 +22,18 @@ db_session = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.htm', user=current_user)
+
+@app.route('/login')
+def login():
+    return render_template('login.htm', user=current_user)
 
 @login_manager.user_loader
 def load_user(userid):
     return db_session.query(model.User).filter(model.User.id == userid).first()
 
-@app.route("/login_local")
+@app.route('/login_local', methods=['GET', 'POST'])
 def login_local():
     if request.method == 'POST':
         username = request.form["username"]
@@ -37,18 +41,18 @@ def login_local():
     elif request.method == 'GET':
         username = request.args.get('username', '')
         password = request.args.get('password', '')
-
+        
     user = db_session.query(model.User_local).filter(model.User_local.name == username).first()
-
+    
     if not user:
         flash('login failed!')
-        return redirect('/')
+        return redirect('/login')
     if user.password == hashlib.sha512(password).hexdigest():
         flash('login successful!')
         login_user(user)
         return redirect('/')
     flash('login failed!')
-    return redirect('/')
+    return redirect('/login')
 
 
 if __name__ == '__main__':
